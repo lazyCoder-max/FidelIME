d1883e888229b2fb1b30aedb5418bdcfe08daca9
daniel <kidusdaniel@outlook.com>
2022-03-31 21:33:59 +0300

    GetSyllable method added

---
+++ FidelIME.Plugin/IME/Interfaces/ISyllableControl.cs
@@ -0,0 +1,8 @@
+﻿namespace FidelIME.Plugin.IME.Interfaces
+{
+    public interface ISyllableControl
+    {
+        string GetSyllable(string value);
+        string ToEthiopic(string value);
+    }
+}
